df3896a347596ad9ee8a2cc432ba87e2f2a1fff9
anagha-mahajan <anagha.mahajan@globallogic.com>
2017-11-14 14:48:31 +0530

    Added Next and Previous button for view-pager

--- ditrp/app/src/main/java/com/lawrence/ditrp/activities/ViewPagerActivity.java
+++ ditrp/app/src/main/java/com/lawrence/ditrp/activities/ViewPagerActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.lawrence.ditrp.Constants.Utils;
 import com.lawrence.ditrp.R;
@@ -15,9 +17,11 @@ import java.util.List;
 /**
  * Created by Anagha.Mahajan on 10-Nov-17.
  */
-public class ViewPagerActivity extends AppCompatActivity {
+public class ViewPagerActivity extends AppCompatActivity implements View.OnClickListener{
 
     public ViewPager viewPager;
+    private Button mNextButton;
+    private Button mPreviousButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,9 +29,33 @@ public class ViewPagerActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_view_pager);
         viewPager = (ViewPager) findViewById(R.id.view_pager);
+        mPreviousButton = (Button)findViewById(R.id.button_previous);
+        mPreviousButton.setOnClickListener(this);
+        mNextButton = (Button)findViewById(R.id.button_next);
+        mNextButton.setOnClickListener(this);
 
         List<QuestionBank> questionBanks = Utils.getQuestionIntoDB(getApplicationContext());
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this, questionBanks);
         viewPager.setAdapter(viewPagerAdapter);
+    }
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_previous:
+                viewPager.setCurrentItem(getItem(-1), true);
+                if (viewPager.getCurrentItem() == 0){
+                    mPreviousButton.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.button_next:
+                viewPager.setCurrentItem(getItem(+1), true);
+                mPreviousButton.setVisibility(View.VISIBLE);
+                break;
+        }
     }
 }
--- ditrp/app/src/main/java/com/lawrence/ditrp/adapter/ViewPagerAdapter.java
+++ ditrp/app/src/main/java/com/lawrence/ditrp/adapter/ViewPagerAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.lawrence.ditrp.Constants.Utils;
 import com.lawrence.ditrp.R;
 import com.lawrence.ditrp.dataModel.QuestionBank;
 
@@ -16,41 +17,50 @@ import java.util.List;
 /**
  * Created by Anagha.Mahajan on 10-Nov-17.
  */
-public class ViewPagerAdapter extends PagerAdapter {
+public class ViewPagerAdapter extends PagerAdapter implements View.OnClickListener {
 
     private Context context;
     private LayoutInflater inflater;
     private List<QuestionBank> questionBanks;
+    ViewHolder holder;
 
     public ViewPagerAdapter(Context context, List<QuestionBank> questionBanks) {
         this.context = context;
         this.questionBanks = questionBanks;
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        holder = new ViewHolder();
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        ViewHolder holder = new ViewHolder();
         View itemView = inflater.inflate(R.layout.viewpager_item, container, false);
 
         holder.view3 = (View) itemView.findViewById(R.id.view3);
         holder.view4 = (View) itemView.findViewById(R.id.view4);
         holder.questionTextView = (TextView) itemView.findViewById(R.id.text_question);
         holder.questionTextView.setText("Q. " + questionBanks.get(position).getQuestion());
+
         holder.answerATextView = (TextView) itemView.findViewById(R.id.text_answer_a);
         holder.answerATextView.setText(questionBanks.get(position).getOptionA());
+        holder.answerATextView.setOnClickListener(this);
+
         holder.answerBTextView = (TextView) itemView.findViewById(R.id.text_answer_b);
         holder.answerBTextView.setText(questionBanks.get(position).getOptionB());
+        holder.answerBTextView.setOnClickListener(this);
+
         holder.answerCTextView = (TextView) itemView.findViewById(R.id.text_answer_c);
         if (!questionBanks.get(position).getOptionC().equals("")) {
             holder.answerCTextView.setText(questionBanks.get(position).getOptionC());
             holder.view3.setVisibility(View.VISIBLE);
         }
+        holder.answerCTextView.setOnClickListener(this);
+
         holder.answerDTextView = (TextView) itemView.findViewById(R.id.text_answer_d);
         if (!questionBanks.get(position).getOptionD().equals("")) {
             holder.answerDTextView.setText(questionBanks.get(position).getOptionD());
             holder.view4.setVisibility(View.VISIBLE);
         }
+        holder.answerDTextView.setOnClickListener(this);
 
         container.addView(itemView);
         return itemView;
@@ -64,12 +74,36 @@ public class ViewPagerAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return questionBanks.size();
+        return 60;
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((LinearLayout) object);
+    }
+
+    @Override
+    public void onClick(View v) {
+       // ViewHolder holder = new ViewHolder();
+        switch (v.getId()){
+            case R.id.text_answer_a:
+                holder.answerATextView.setBackgroundResource(R.color.colorPrimaryDark);
+                Utils.showToast(context, "ANSWER A");
+
+                break;
+            case R.id.text_answer_b:
+                Utils.showToast(context, "ANSWER B");
+                holder.answerBTextView.setBackgroundResource(R.color.colorPrimaryDark);
+                break;
+            case R.id.text_answer_c:
+                Utils.showToast(context, "ANSWER C");
+                holder.answerCTextView.setBackgroundResource(R.color.colorPrimaryDark);
+                break;
+            case R.id.text_answer_d:
+                Utils.showToast(context, "ANSWER D");
+                holder.answerDTextView.setBackgroundResource(R.color.colorPrimaryDark);
+                break;
+        }
     }
 
     public class ViewHolder {
